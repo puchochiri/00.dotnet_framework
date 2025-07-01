633d706c8994c5b0fd713cab8a5c356057c391b9
puchochiri <doawishfor@naver.com>
2025-07-01 22:57:19 +0900

    commit message

---
+++ FirstCon1/Program.vb
@@ -0,0 +1,14 @@
+Using System;
+Using System.Collections.Generic;
+
+
+
+Namespace FirstCon1
+    {
+        Class Program
+        {
+            Static void Main(String[] args)
+        }
+    End Class
+    }
+End Namespace
