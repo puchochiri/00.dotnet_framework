f71dda3c694f1a74b47498831f5b6513f786c78b
puchochiri <doawishfor@naver.com>
2025-07-03 23:09:45 +0900

    commit message

---
+++ FirstWinform_dotNetFramwork_01/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FirstWinform_dotNetFramwork_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = "사과";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Text = "오렌지";
+        }
+    }
+}
